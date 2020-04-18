57ab9ff0f6e1201b358bfaf27135e4e7793d4787
Martin Silverio <msilverio324@gmail.com>
2020-04-18 13:27:04 -0700

    Split homepage into components, added styling

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory';
+import './Homepage.scss';
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+}
+
+export default Homepage;
