c7944272be8cd9a53565902d13070983aef9b202
Martin Silverio <msilverio324@gmail.com>
2020-04-20 15:39:49 -0700

    Added early parts of header and styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
