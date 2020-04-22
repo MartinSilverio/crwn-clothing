67f4ce2961054163981e75cd98598664458afebe
Martin Silverio <msilverio324@gmail.com>
2020-04-22 16:04:52 -0700

    Added ability to remove and adjust quantity of products in cart

--- src/components/checkout-item/CheckoutItem.js
+++ src/components/checkout-item/CheckoutItem.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from '../../redux/cart/cartActions';
 import './CheckoutItem.scss';
 
-function CheckoutItem({ cartItem: { name, imageUrl, price, quantity } }) {
+function CheckoutItem({ cartItem, clearItem, addItem, removeItem }) {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
       <div className='image-container'>
         <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>${price}</span>
-      <div className='remove-button'>&#10006;</div>
+      <div className='remove-button' onClick={() => clearItem(cartItem)}>
+        &#10006;
+      </div>
     </div>
   );
 }
-export default CheckoutItem;
+
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (cartItem) => dispatch(clearItemFromCart(cartItem)),
+  addItem: (cartItem) => dispatch(addItem(cartItem)),
+  removeItem: (cartItem) => dispatch(removeItem(cartItem)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cartSelectors.js
+++ src/redux/cart/cartSelectors.js
@@ -23,7 +23,8 @@ export const selectCartItemsCount = createSelector(
 );
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.price,
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.price * cartItem.quantity,
     0
   )
 );
