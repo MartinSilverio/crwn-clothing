90ad99b2c89da70c8b06b6379fdd278ebe5a312d
Martin Silverio <msilverio324@gmail.com>
2020-04-22 19:44:54 -0700

    Added collection page to show individual categories

--- src/components/collections-overview/CollectionsOverview.js
+++ src/components/collections-overview/CollectionsOverview.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/CollectionPreview';
-import { selectCollections } from '../../redux/shop/shopSelectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shopSelectors';
 import './CollectionsOverview.scss';
 
 function CollectionsOverview({ collections }) {
@@ -17,7 +17,7 @@ function CollectionsOverview({ collections }) {
 }
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/CollectionsOverview';
+import CollectionPage from '../collection/CollectionPage';
 
-function Shop() {
+function Shop({ match }) {
   return (
-    <div>
-      <CollectionOverview />
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 }
