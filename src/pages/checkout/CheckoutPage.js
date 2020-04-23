37fcce535e52bd30e5a31a2d293f7ea3731d4e43
Martin Silverio <msilverio324@gmail.com>
2020-04-22 20:32:32 -0700

    Added stripe payment testing, changed title and favicon

--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CheckoutItem from '../../components/checkout-item/CheckoutItem';
+import StripeCheckoutButton from '../../components/stripe-button/StripeButton';
 
 import { createStructuredSelector } from 'reselect';
 import {
@@ -37,6 +38,12 @@ function CheckoutPage({ cartItems, total }) {
       <div className='total'>
         <span>TOTAL: ${total}</span>
       </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 09/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 }
